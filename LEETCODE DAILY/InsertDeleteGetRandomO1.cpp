737647be66b133dd6a6749ff41ed35c5625bec7c
Twinshu <twinshup@gmail.com>
2022-11-29 23:39:30 +0530

    LC daily

---
+++ LEETCODE DAILY/InsertDeleteGetRandomO1.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+class RandomizedSet {
+public:
+    RandomizedSet() {
+        
+    }
+    
+    bool insert(int val) {
+        
+    }
+    
+    bool remove(int val) {
+        
+    }
+    
+    int getRandom() {
+        
+    }
+};
+
+
+int main()
+{
+
+RandomizedSet* obj = new RandomizedSet();
+bool param_1 = obj->insert(8);
+bool param_2 = obj->remove(2);
+int param_3 = obj->getRandom();
+ 
+    return 0;
+}
