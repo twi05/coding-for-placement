b885c781c3fa6b507626748ce1819d05c82e420e
Twinshu <twinshup@gmail.com>
2022-12-01 23:54:13 +0530

    LC daily

---
+++ LEETCODE DAILY/December/DetermineIfStringsAreAlike.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{
+
+    return 0;
+}
+
+bool halvesAreAlike(string s)
+{
+    int n = s.size();
+    int a = 0, b = 0;
+    vector<char> voy = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+    for (int i = 0; i < n / 2; i++)
+    {
+        for (auto x : voy)
+        {
+            if (s[i] == x)
+            {
+                a++;
+                break;
+            }
+        }
+    }
+    for (int i = n / 2; i < n; i++)
+    {
+        for (auto x : voy)
+        {
+            if (s[i] == x)
+            {
+                b++;
+                break;
+            }
+        }
+    }
+    return a == b;
+}
