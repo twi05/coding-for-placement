34e03d3d6da57da49b432026769ef98bc4278607
Twinshu <twinshup@gmail.com>
2022-12-25 00:10:31 +0530

    LC daily

---
+++ LEETCODE DAILY/December/DominoandTriminoTiling.cpp
@@ -0,0 +1,17 @@
+ #include<bits/stdc++.h>
+ using namespace std;
+ typedef long long ll;
+    int numTilings(int N) {
+        int a = 0, b = 1, c = 1, c2, mod = 1e9 + 7;
+        while (--N) {
+            c2 = (c * 2 % mod + a) % mod;
+            a = b;
+            b = c;
+            c = c2;
+        }
+        return c;
+    }
+ int main(){
+ 
+ return 0;
+ }
