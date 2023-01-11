8e365e48bef8212f57d704ec65c47d98bcc9b720
Twinshu <twinshup@gmail.com>
2023-01-11 23:31:50 +0530

    LC Daily

---
+++ LEETCODE DAILY/January/MinimumTimeToCollectAllApplesInTree.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int minTime(int n, vector<vector<int>> &edges, vector<bool> &hasApple)
+{
+}
+
+int main()
+{
+
+    return 0;
+}
